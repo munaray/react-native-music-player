9b70fbe4cfd6ab5d05deb67976a55e5177362e14
munaray <munahdesigns@gmail.com>
2024-07-28 11:27:18 +0100

    fixig eas.json for CI

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,27 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App = () => {
+	const onFetchUpdateAsync = async () => {
+		try {
+			const update = await Updates.checkForUpdateAsync();
+
+			if (update.isAvailable) {
+				await Updates.fetchUpdateAsync();
+				await Updates.reloadAsync();
+			}
+		} catch (error) {
+			alert(`Error fetching latest Expo update: ${error}`);
+		}
+	};
+
+	useEffect(() => {
+		onFetchUpdateAsync;
+	}, []);
+
 	return (
 		<SafeAreaProvider>
 			<RootNavigation />
